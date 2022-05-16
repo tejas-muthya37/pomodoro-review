2364ec30aa895993eafed08b889f2d9b4a34948f
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2022-05-16 10:56:32 +0530

    Feat: Configured App and index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./Screens/Homepage/Homepage";
+import Task from "./Screens/Task/Task";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/task/:taskId" element={<Task />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { PomodoroProvider } from "./Context/pomodoro-context";
+import { TaskProvider } from "./Context/task-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <PomodoroProvider>
+      <TaskProvider>
+        <App />
+      </TaskProvider>
+    </PomodoroProvider>
   </React.StrictMode>
 );
